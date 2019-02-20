40a504a2f842068004531853e5c1585f9631142f
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2019-02-20 14:24:26 +0500

    add config to endpoing

--- macrobase_driver/endpoint.py
+++ macrobase_driver/endpoint.py
@@ -1,16 +1,17 @@
 import abc
 
 from macrobase_driver.context import Context
+from macrobase_driver.config import BaseConfig
 
 
 class Endpoint(object, metaclass=abc.ABCMeta):
     """
     Endpoint protocol for processing from macrobase and his drivers
     """
-    context: Context
 
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: BaseConfig):
         self.context = context
+        self.config = config
         self.__name__ = self.__class__.__name__
 
     async def __call__(self, *args, **kwargs):
