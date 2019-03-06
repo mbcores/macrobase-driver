398bdd18c16bbfb6a68536cc8ed238703b47d2d0
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2019-03-06 21:52:57 +0500

    refactor constructor

--- macrobase_driver/driver.py
+++ macrobase_driver/driver.py
@@ -1,13 +1,20 @@
+from asyncio import AbstractEventLoop, get_event_loop
 from typing import ClassVar
 
+from macrobase_driver.context import Context
 from macrobase_driver.config import DriverConfig
 
 
 class MacrobaseDriver(object):
 
-    def __init__(self, name: str = None):
+    def __init__(self, name: str = None, *args, **kwargs):
         self.name = name
         self.config = DriverConfig
+        self.context = Context()
+
+    @property
+    def loop(self) -> AbstractEventLoop:
+        return get_event_loop()
 
     def update_config(self, config_obj: ClassVar[DriverConfig]):
         pass
