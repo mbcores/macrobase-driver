e29bf90adf15861f045644eeef33162dd3563174
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2020-05-19 17:51:20 +0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='macrobase_driver',
-    version='2.1.1',
+    version='2.1.2',
     packages=find_packages(),
     url='https://github.com/mbcores/macrobase-driver',
     license='MIT',
